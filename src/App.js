ea187bc480a4d81cfc707cf29fa0fd9899d15899
samreen-2021 <determinedlearner2019@gmail.com>
2021-08-31 14:47:54 +0500

    Contact Listing App using React Js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const[userData, setUserData] = useState([]);
+  const[userAge, setUserAge] = useState("");
+  useEffect(() => {
+      fetch('https://randomuser.me/api/?results=9')
+      .then(results => results.json())
+      .then(data =>{
+          console.log(data)
+          setUserData(data.results);
+      });
+  },[])
+  return(
+    <>
+    <div  style={{textAlign: 'center', fontSize: '46px'}}>Contact Listing App</div>
+      {
+      userData.map((user) => (
+        <div className="card mb-3 mx-auto" style={{width: '600px', backgroundColor: 'aqua', margin: '45px', padding:'30px'}}>
+         <div className="row g-0" >
+          <div className="col-md-4">    
+            <img src={user.picture.large} alt="user-img"></img>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body" style={{color: 'purple'}} >
+              <h5 className="card-name">Name: {user.name.first} {user.name.last} </h5>
+              <h5 className='card-Gender'>Gender: {user.gender} </h5>
+              <h5 className='card-email'>Email: {user.email} </h5>
+              <h5 className='card-age'>Age: {userAge} </h5>
+              <p className='text-center'><button className="btn btn-primary" onClick = {() => setUserAge(user.dob.age)}>Toggle Age</button></p>
+            </div>
+          </div>
+        </div>
+        </div>
+      
+      
+      ))}
+    </>
+);}
+
 
 export default App;
